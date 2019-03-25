b4bff03d03bfa5809574ea1f733558b4e8297849
ana <anadedavid@gmail.com>
2019-03-25 20:14:30 -0300

    first commit

---
+++ PL/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioEFCoreCodeFirst.PL
+{
+    public class Genre
+    {
+        public int GenreID { get; set; } //chave primaria - anotacao [Key]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Movie> Movies { get; set; } 
+    }
+}
---
+++ PL/MovieContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioEFCoreCodeFirst.PL
+{
+    class MovieContext : DbContext
+    {
+        //      public static readonly LoggerFactory FabricaLogger = new LoggerFactory(new[] { new
+        //ConsoleLoggerProvider((_, __) => true, true) });
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+           // .UseLoggerFactory(FabricaLogger)
+
+           .UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=DBMovieCF;Trusted_Connection=True;");
+        }
+    }
+}
